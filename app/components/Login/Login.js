6a87227a003605288faed19c45190ba4d0a37934
jinvi <jinvi@qq.com>
2018-06-03 12:18:34 +0800

    update

--- app/components/Login/Login.js
+++ app/components/Login/Login.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import {fetchJSON} from '../../utils/fetch'
-import Loading from '../common/loading'
 
 export default class Main extends Component {
     constructor(props) {
@@ -12,34 +11,30 @@ export default class Main extends Component {
     }
 
     render() {
-        const _this = this
-        return 1 ?
-            (
-                <div className={'login-container'}>
-                    <div className={'login'}>
-                        <div className={'login-title'}>用户登录</div>
-                        <form>
-                            <div>
-                                <input className={'login-text' + _this.state.isActive} placeholder={'Access Token'}
-                                       type={'text'}
-                                       onClick={() => {
-                                           _this.setState({
-                                               isActive: ' active'
-                                           })
-                                       }}
-                                       onBlur={() => {
-                                           _this.setState({
-                                               isActive: ''
-                                           })
-                                       }}/>
-                            </div>
-                            <button className={'login-btn'}>登录</button>
-                        </form>
-                        <div className={'login-detail'}>Access Token 在设置页面获取</div>
-                    </div>
+        return (
+            <div className={'login-container'}>
+                <div className={'login'}>
+                    <div className={'login-title'}>用户登录</div>
+                    <form>
+                        <div>
+                            <input className={'login-text' + this.state.isActive} placeholder={'Access Token'}
+                                   type={'text'}
+                                   onClick={() => {
+                                       this.setState({
+                                           isActive: ' active'
+                                       })
+                                   }}
+                                   onBlur={() => {
+                                       this.setState({
+                                           isActive: ''
+                                       })
+                                   }}/>
+                        </div>
+                        <button className={'login-btn'}>登录</button>
+                    </form>
+                    <div className={'login-detail'}>Access Token 在设置页面获取</div>
                 </div>
-            )
-            :
-            <Loading/>
+            </div>
+        )
     }
 }
