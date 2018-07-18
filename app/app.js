9f37f00bee8d93655b62aa120f94d5dc20630595
jun <jun@linseek.com>
2018-07-18 16:27:08 +0800

    兼容Object.assign

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {render} from "react-dom";
 import {BrowserRouter} from 'react-router-dom'
+import assign from 'core-js/features/object/assign'  //兼容Object.assign
 import App from './components/route'
 import {Provider} from 'react-redux'
 import store from './store/store'
