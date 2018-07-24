ce36df4945929bd8431fd5b7c08fb2b9eb19ee5e
jinvi <jinvi@qq.com>
2018-07-24 19:13:10 +0800

    update

--- app/app.js
+++ app/app.js
@@ -2,11 +2,11 @@ import React from "react";
 import {render} from "react-dom";
 import {BrowserRouter} from 'react-router-dom'
 import 'core-js/features/object/assign'  //兼容Object.assign
+import 'es6-promise/auto'  //promise polyfill
+import 'whatwg-fetch'  //fetch polyfill
 import App from './components/route'
 import {Provider} from 'react-redux'
 import store from './store/store'
-import 'es6-promise/auto'  //promise polyfill
-import 'whatwg-fetch'  //fetch polyfill
 
 import 'normalize.css'
 import './style/index.less'
