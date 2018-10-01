f61afafd14ed10dc8e89cb4a99e42c49831ca58d
jinvi <jinvi@qq.com>
2018-10-01 12:29:49 +0800

    u

--- app/app.js
+++ app/app.js
@@ -14,8 +14,9 @@ import './style/index.less'
 // import vConsole from 'vconsole'  //移动端控制台，需new创建实例出来，不用时请注释
 // new vConsole();
 
-//使用service worker在客户端会把文件都缓存，更新线上文件时必须同时更新客户端缓存文件，
-//所以更新线上文件必须触发更新 sw.js 文件以更新缓存文件（注意只更改样式文件并不会触发更新sw.js文件）
+//使用service worker在客户端会把预缓存文件都缓存，更新线上这些文件时必须同时更新客户端缓存文件，
+//所以更新这些线上文件必须触发更新 sw.js 文件以更新缓存文件
+// 注意：只更改样式文件，输出并不会触发更新 sw.js 文件
 require('./service-worker/registration')()  //service worker注册
 
 render((
